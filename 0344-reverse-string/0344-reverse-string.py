20554414300e5772aa70692fa513083fab891308
KRUSHNA DODKE <159526140+KrushnaD7@users.noreply.github.com>
2026-07-27 14:49:38 +0530

    Time: 0 ms (100%), Space: 23.3 MB (80.07%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        f = 0 
+        l = len(s) - 1 
+        
+        while f < l:
+            s[f] , s[l] = s[l] , s[f]
+            f = f + 1
+            l = l - 1
+        
