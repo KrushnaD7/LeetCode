6f7c31c03ec2eadef7a2597c4ebfce28dc4b10dc
KRUSHNA DODKE <159526140+KrushnaD7@users.noreply.github.com>
2026-07-28 15:17:54 +0530

    Time: 15 ms (44.35%), Space: 20.2 MB (18.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,26 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # return str(list(s).sort()) == str(list(t).sort()) just trying to solve in one line but ...
+        return sorted(s) == sorted(t)
 
-        a = {}
-        b = {}
+        # a = {}
+        # b = {}
 
-        if len(s) != len(t):
-            return False  
+        # if len(s) != len(t):
+        #     return False  
 
-        for i in range(len(s)):
-            if s[i] not in a:
-                a[s[i]] = 1
-            if s[i] in a:
-                a[s[i]] += 1
+        # for i in range(len(s)):
+        #     if s[i] not in a:
+        #         a[s[i]] = 1
+        #     if s[i] in a:
+        #         a[s[i]] += 1
 
-            if t[i] not in b:
-                b[t[i]] = 1
-            if t[i] in b:
-                b[t[i]] += 1
+        #     if t[i] not in b:
+        #         b[t[i]] = 1
+        #     if t[i] in b:
+        #         b[t[i]] += 1
 
-        print(a,b)
-        return a == b
+        # print(a,b)
+        # return a == b
 
 
 
