07ab67cdf8a07b48f924682cdc775b006423a6c5
KRUSHNA DODKE <159526140+KrushnaD7@users.noreply.github.com>
2026-07-28 11:38:57 +0530

    Time: 7 ms (27.04%), Space: 20.4 MB (75.32%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,27 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        a = {}
+        # a = {}
 
-        for i in range(len(numbers)):
-            if (target - numbers[i]) in a:
-                return [a[target - numbers[i]]+1, i+1]
+        # for i in range(len(numbers)):
+        #     if (target - numbers[i]) in a:
+        #         return [a[target - numbers[i]]+1, i+1]
 
-            a[numbers[i]] = i
+        #     a[numbers[i]] = i  this work but we go for more optimal 
+
+
+        # let solve with two pointer methon here list is order so we consider 2 pointer left and right and move them according 
+
+        l = 0
+        r = len(numbers) - 1
+
+        while l <r:
+
+            t = numbers[l] + numbers[r]
+
+            if t == target:
+                return [l+1, r+1]
+            elif t < target:
+                l+=1
+            else:
+                r-=1
         
