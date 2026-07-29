5aa4b8fb1a412e42a2e43f910a79ad2936c6d2a3
KRUSHNA DODKE <159526140+KrushnaD7@users.noreply.github.com>
2026-07-29 10:50:56 +0530

    Time: 11 ms (82.62%), Space: 21.7 MB (92.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,35 +1,14 @@
 class Solution:
-    
-    # def sor(self,o):
-        
-    #     sorted(o)
-    #     print(o)
-    #     return str(o)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-    #     a = {}
+        a = {}
 
-    #     for s in strs:
-    #         key = self.sor(s)
+        for i in strs:
+            k = "".join(sorted(i))
 
-    #         if a[key] not in a:
-    #             a[key] = s
-    #         else:
-    #             a[key].append(s)
-
-
-    #     return list[a.values()]
-
-        groups = {}
-
-        for s in strs:
-            key = "".join(sorted(s))
-
-            if key not in groups:
-                groups[key] = []
-
-            groups[key].append(s)
-
-        return list(groups.values())
-
+            if k not in a:
+                a[k] = [i]
+            else:
+                a[k].append(i)
+        return list(a.values())
         
